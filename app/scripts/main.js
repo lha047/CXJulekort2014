0d651267ded19da6d4881b8a5293ba1be950f383
lha <pur3st2r>
2014-12-09 21:26:11 +0100

    fiksa på plassering på lykta

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -98,6 +98,13 @@ var s = skrollr.init({
 window.onload = function(){
         function resize() {
             var viewportWidth = $(window).width();
+            var viewportHeight = $(window).height();
+
+            if(viewportWidth < viewportHeight) {
+                $('#lykt').css({'background-position': 5 + 'em'});
+                $('#lykt-opplyst').css({'background-position': 5 + 'em'});
+                console.log('mindre');
+            } 
 
             $('#slides').css({width: $('#slide4').height() * 3.55 + 'px'});
             $('#slide4verscontainer').css({width: $('#slide4').height() * 3.55 + 'px'});
@@ -197,7 +204,6 @@ window.onload = function(){
             akeBilde.attr('data-'+ akeSluttScroll +'p', 'left[x]:'+akeSluttLeft+'%;top[y]:'+ akeSluttTop+'%');
             // akeBilde.attr('data-'+ (akeSluttScroll +50) +'p', 'left[x]:'+(akeSluttLeft+5)+'%;top[y]:'+ (akeSluttTop)+'%');
 
-           	console.log("log left: "+akeBilde.css("left"));
 
             var imageWidth = $('#slides').width();
 
