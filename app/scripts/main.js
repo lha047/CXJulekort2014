d85542cbad2d5a2a201b47c9d8364cfed35382ed
lha <pur3st2r>
2014-12-10 19:18:58 +0100

    skriveleif + like translate versjoner

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -227,7 +227,7 @@ window.onload = function(){
             var fontSize = (pp * baseSize) + 'px';
             $('.intro-textbox').css({'font-size': fontSize});
 
-            $('.scollText').animate({ fontSize: "1.2em" }, 10000);
+            $('.scrollText').animate({ fontSize: "1.2em" }, 10000);
 
             skrollr.get().refresh();
 
